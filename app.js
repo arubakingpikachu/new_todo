d393f375f291d2b391f0c257df5e4cdfe6f3972d
arubakingpikachu <85471527+arubakingpikachu@users.noreply.github.com>
2023-04-25 01:01:27 +0800

    feat: add delete todo function

--- app.js
+++ app.js
@@ -77,6 +77,14 @@ app.put('/todos/:id',(req,res)=>{
   .catch(error => console.log(error))
 })
 
+app.delete('/todos/:id',(req,res)=>{
+  const id=req.params.id
+  return Todo.findById(id)
+  .then((todo)=>todo.remove())
+  .then(()=>res.redirect('/'))
+  .catch(error => console.log(error))
+})
+
 app.listen(3000,()=>{
   console.log('App is running on http://localhost:3000')
 })
