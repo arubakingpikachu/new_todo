fd0f3bb5df77a5c3c265d41528ec3636f7636de0
arubakingpikachu <85471527+arubakingpikachu@users.noreply.github.com>
2023-04-25 02:33:31 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,28 +1,13 @@
 const express=require('express')
 const exphbs = require('express-handlebars')
 const Todo=require('./models/todo')
-const mongoose = require('mongoose')
 const routes=require('./routes')
 const methodOverride = require('method-override') 
+require('./config/mongoose')
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
+
 
 const app =express()
-
-
-
-mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
--- models/seeds/todoSeeders.js
+++ models/seeds/todoSeeders.js
@@ -1,15 +1,8 @@
-const mongoose=require('mongoose')
+
 const Todo=require('../todo')
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
+const db=require('../../config/mongoose')
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
 db.once('open', () => {
   console.log('mongodb connected!')
   for(let i=0;i<10;i++){
