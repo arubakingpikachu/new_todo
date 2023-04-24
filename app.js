1776323aaffb195642812b9125d66c4eeb551d37
arubakingpikachu <85471527+arubakingpikachu@users.noreply.github.com>
2023-04-24 20:46:00 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-const express=require('express') 
+const express=require('express')
+const mongoose = require('mongoose')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+
 const app =express()
-
-
+mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 
 app.listen(3000,()=>{
