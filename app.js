e35cdf5780e26f43260e065e1715dbb6f9ec0bde
arubakingpikachu <85471527+arubakingpikachu@users.noreply.github.com>
2023-04-24 22:47:12 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express=require('express')
 const exphbs = require('express-handlebars')
+const Todo=require('./models/todo')
 const mongoose = require('mongoose')
 
 if (process.env.NODE_ENV !== 'production') {
@@ -25,7 +26,10 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  Todo.find()
+  .lean()
+  .then(todos=>res.render('index',{todos:todos}))
+  .catch(error => console.error(error))
 })
 
 app.listen(3000,()=>{
