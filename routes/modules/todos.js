8a02ce7b612ab385aa06a61d09f37cd039101ad8
arubakingpikachu <85471527+arubakingpikachu@users.noreply.github.com>
2023-04-25 14:29:21 +0800

    feat: modify index page ui

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -32,10 +32,11 @@ router.get('/:id/edit',(req,res)=>{
 
 router.put('/:id',(req,res)=>{
   const id=req.params.id
-  const name=req.body.name
+  const {name,isDone}=req.body
   return Todo.findById(id)
   .then((todo)=>{
     todo.name=name
+    todo.isDone = isDone === 'on'
     return todo.save()
   })
   .then(()=>{
