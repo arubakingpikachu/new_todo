2db5ed5d3fbd93b405a4fa498501aa6c5f0a53e7
arubakingpikachu <85471527+arubakingpikachu@users.noreply.github.com>
2023-04-25 17:58:02 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -3,6 +3,12 @@ const router=express.Router()
 
 router.get('/login', (req, res) => {
   res.render('login')
+})//燈入夜面
+
+router.post('/login',(req,res)=>{
+})
+router.get('/register',(req,res)=>{
+  res.render('register')
 })
 
 module.exports = router
