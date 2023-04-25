deef807d6e73d5c3839175ef661303539a4d5ee3
arubakingpikachu <85471527+arubakingpikachu@users.noreply.github.com>
2023-04-25 17:32:35 +0800

    feat: add user modules

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -6,6 +6,7 @@ const Todo = require('../../models/todo')
 router.get('/', (req, res) => {
   Todo.find()
   .lean()
+  .sort({ _id: 'asc' })
   .then(todos=>res.render('index',{todos:todos}))
   .catch(error=> console.error(error)) 
 })
